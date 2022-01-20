9251486f182acbaa98e74d33247084c38b012872
Ian S <iansilber32@gmail.com>
2022-01-20 15:17:51 -0500

    udemy python more conditionals

--- conditionals.py
+++ conditionals.py
@@ -19,18 +19,35 @@ else:
     print("We dont serve that here")
 '''
 
-num_1, operator, num_2 = input("enter calculation: ").split()
+# simple calculator
+# num_1, operator, num_2 = input("enter calculation: ").split()
 
-num_2 = int(num_2)
-num_1 = int(num_1)
+# num_2 = int(num_2)
+# num_1 = int(num_1)
 
-if operator == "+":
-    print(num_1 + num_2)
-elif operator == "-":
-    print(num_1 - num_2)
-elif operator == "*":
-    print(num_1 * num_2)
-elif operator == "/":
-    print(num_1 / num_2)
+# if operator == "+":
+#     print(num_1 + num_2)
+# elif operator == "-":
+#     print(num_1 - num_2)
+# elif operator == "*":
+#     print(num_1 * num_2)
+# elif operator == "/":
+#     print(num_1 / num_2)
+# else:
+#     print("not a valid calculation")
+'''
+age = int(input("What is your age: "))
+can_vote = True if age >= 18 else False  # single line conditional logic
+print("You can vote :", can_vote)
+'''
+
+age = 3
+if age < 5:
+    print("Too Young for School")
+elif age == 5:
+    print("Go to kindergarten")
+elif (age > 5) and (age <= 17):
+    grade = age - 5
+    print("Go to Grade ", grade)
 else:
-    print("not a valid calculation")
+    print("Go to College")
